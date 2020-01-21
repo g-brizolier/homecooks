c1a239d0e54cf99f84a5020e22e0aebc545417a9
chaoticdenim <gbrizolier@gmail.com>
2020-01-21 10:13:50 +0100

    release build

---
+++ build/precache-manifest.223480d669f1263d0435a43a38cb5fd3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb7c7c1241a7aa6737282468e0e92b8",
+    "url": "/homecooks/index.html"
+  },
+  {
+    "revision": "0be786c618b29326b467",
+    "url": "/homecooks/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "fcec0f835e79c7051b83",
+    "url": "/homecooks/static/js/2.69f89c5e.chunk.js"
+  },
+  {
+    "revision": "0be786c618b29326b467",
+    "url": "/homecooks/static/js/main.a4cb18cf.chunk.js"
+  },
+  {
+    "revision": "e98533de6c531865856d",
+    "url": "/homecooks/static/js/runtime~main.4398e068.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/homecooks/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e545f60fc5475bc3b3a716a8d39c9d92",
+    "url": "/homecooks/static/media/bg-pasta.e545f60f.jpg"
+  },
+  {
+    "revision": "ef542092198b4c651079f381b8e31249",
+    "url": "/homecooks/static/media/bg-sushi.ef542092.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/homecooks/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/homecooks/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "0ee0a61e38447ec9f89a7971a624bec4",
+    "url": "/homecooks/static/media/bg8.0ee0a61e.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/homecooks/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/homecooks/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/homecooks/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/homecooks/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/homecooks/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/homecooks/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/homecooks/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "b7003c4c348398312c25f052f3dc4d19",
+    "url": "/homecooks/static/media/login.b7003c4c.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/homecooks/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/homecooks/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/homecooks/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/homecooks/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/homecooks/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/homecooks/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/homecooks/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/homecooks/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/homecooks/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/homecooks/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
